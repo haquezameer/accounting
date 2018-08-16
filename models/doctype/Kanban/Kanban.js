e59cb7eedd4373ed4df2f3fb9026865097326b07
zamhaq <haque864@gmail.com>
2018-08-16 20:01:05 +0530

    Add Kanban model

---
+++ models/doctype/Kanban/Kanban.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: 'Kanban',
+  doctype: 'DocType',
+  naming: 'autoincrement',
+  isSingle: 0,
+  isChild: 0,
+  "keywordFields": [],
+  "fields": [
+    {
+      "fieldname": "kanbanName",
+      "label": "Kanban Name",
+      "fieldtype": "Data",
+      "required": 1
+    },
+    {
+      "fieldname": "referencedoctype",
+      "label": "Reference Doctype",
+      "fieldtype": "Select",
+      "options": ["ToDo"],
+      "default": "ToDo",
+      "required": 1
+    }
+  ]
+};
